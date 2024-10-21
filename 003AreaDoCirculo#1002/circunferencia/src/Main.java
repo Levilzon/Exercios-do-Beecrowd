a761b1fd671960fa3aff124f6bf1794f57f93062
Jose Levi Gonçalves de Lima Menezes <levilima2002@gmail.com>
2024-10-21 18:23:03 -0300

    exercício 1040 finalizado

--- 003AreaDoCirculo#1002/circunferencia/src/Main.java
+++ 003AreaDoCirculo#1002/circunferencia/src/Main.java
@@ -1,6 +1,3 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) {
     double n = 3.14159;
--- 026media3#1040/src/Main.java
+++ 026media3#1040/src/Main.java
@@ -4,44 +4,52 @@ import java.io.InputStreamReader;
 import java.text.DecimalFormat;
 
 public class Main {
+    public static final int A = 2;
+    public static final int B = 3;
+    public static final int C = 4;
+    public static final int D = 1;
+
     public static void main(String[] args) throws IOException {
+
+        DecimalFormat df = new DecimalFormat("##.#");
+
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader buffer = new BufferedReader(input);
 
-        DecimalFormat df = new DecimalFormat("#.#");
-
         String[] notas = buffer.readLine().split(" ");
-        double a = Double.parseDouble(notas[0]);
-        double b = Double.parseDouble(notas[1]);
-        double c = Double.parseDouble(notas[2]);
-        double d = Double.parseDouble(notas[3]);
+        double nota1 = Double.parseDouble(notas[0]);
+        double nota2 = Double.parseDouble(notas[1]);
+        double nota3 = Double.parseDouble(notas[2]);
+        double nota4 = Double.parseDouble(notas[3]);
 
-        double media = (a*2 + b*3 + c*4 + d*1)/10;
-
-        if(media < 5){
-            System.out.println("Media: "+ df.format(media));
-            System.out.println("Aluno reprovado.");
-
+        double media = (A*nota1 + B*nota2 + C*nota3 + D*nota4)/10;
+        
+        if(media >= 7){
+            System.out.printf("Media: %.1f\n", media);
+            System.out.println("Aluno aprovado.");
         }
-        else if(media > 5 && media < 7){
-
+        else if(media<7 && media >=5){
+            System.out.printf("Media: %.1f\n", media);
+            System.out.println("Aluno em exame.");
             double exame = Double.parseDouble(buffer.readLine());
             double mediaFinal = (media+exame)/2;
 
-            if(mediaFinal < 5){
-                System.out.println("Aluno reprovado.");
-            }
-            else{
-                System.out.println("Media: "+ df.format(media));
-                System.out.println("Aluno em exame.");
+            if(mediaFinal >=5){
                 System.out.printf("Nota do exame: %.1f\n", exame);
                 System.out.println("Aluno aprovado.");
-                System.out.println("Media final: "+ df.format(mediaFinal));
+                System.out.printf("Media final: %.1f\n", mediaFinal);
             }
+            else{
+                System.out.printf("Nota do exame: %.1f\n", exame);
+                System.out.println("Aluno reprovado.");
+                System.out.printf("Media final: %.1f\n", mediaFinal);
+                
+            }   
+
         }
         else{
-            System.out.printf("Media: %.1f\n", media);
-            System.out.println("Aluno aprovado.");
+            System.out.println("Media: "+df.format(media));
+            System.out.println("Aluno reprovado.");
         }
     }
 }
